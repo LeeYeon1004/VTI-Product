7b60c8145744d990592caebba47cdbbec4cd7977
Trần Tuấn Đạt <76252195+trantuandatvietnam@users.noreply.github.com>
2022-08-24 13:14:33 +0700

    update

--- src/app/app.module.tsx
+++ src/app/app.module.tsx
@@ -4,23 +4,41 @@ import AppRouting from './app-routing';
 import { AppContext } from './app.context';
 import './app.style.scss';
 import LoaderComponent from './core/components/loading/loader.component';
+import ToastComponent from './shared/components/toast/toast.component';
+import { IToastData } from './models/app-context.model';
 
 export default function App() {
   const [showLoader, setShowLoader] = useState<boolean>(false);
+  const [toastMessage, setToastMessage] = useState<IToastData>({
+    show: false,
+    message: '',
+    type: 'warning',
+  });
 
   const onShowLoader = (value: boolean) => {
     setShowLoader(value);
+  };
+
+  const setToastData = (toastData: IToastData) => {
+    setToastMessage(toastData);
+  };
+
+  const getToastData = () => {
+    return toastMessage;
   };
 
   return (
     <AppContext.Provider
       value={{
         onShowLoader,
+        setToastData,
+        getToastData,
       }}
     >
       <BrowserRouter>
         <AppRouting />
         {showLoader && <LoaderComponent />}
+        {toastMessage.show && <ToastComponent />}
       </BrowserRouter>
     </AppContext.Provider>
   );
--- src/app/models/app-context.model.ts
+++ src/app/models/app-context.model.ts
@@ -1,3 +1,11 @@
 export interface AppContextModel {
   onShowLoader: (value: boolean) => void;
+  setToastData: (data: IToastData) => void;
+  getToastData: () =>  IToastData
+}
+
+export interface IToastData {
+  show: boolean,
+  message: string,
+  type: "warning" | "error" | "info" | "success"
 }
